eebbaf43902c75ab0009800f5f49eee1aa450801
Kyle Lynch <kylelynch104@gmail.com>
2019-06-06 12:06:06 -0800

    version_0.1.0

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,27 +1,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import VideoLibrary from '@material-ui/icons/VideoLibrary';
+import Button from '@material-ui/core/Button';
+import Delete from '@material-ui/icons/Delete';
+import Create from '@material-ui/icons/Create';
+import Fab from '@material-ui/core/Fab';
 
 class StreamList extends Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div>
+          <Button variant="outlined" style={{ color: 'grey' }}>
+            Edit
+            <Create color="action" />
+          </Button>
+          <Button variant="outlined" color="secondary">
+            Delete
+            <Delete />
+          </Button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
-        <ListItem button key={stream.id}>
+        <ListItem key={stream.id} style={{ marginTop: '0.5rem' }}>
           <VideoLibrary />
-          <ListItemText>{stream.title}</ListItemText>
-          <ListItemText>{stream.description}</ListItemText>
+          <ListItemText
+            primary={stream.title}
+            secondary={stream.description}
+            style={{ padding: '0 1rem' }}
+          />
+          {this.renderAdmin(stream)}
         </ListItem>
       );
     });
+  }
+
+  renderCreate() {
+    if (this.props.isSignedIn === true) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Fab variant="extended" size="small" color="primary">
+            <Link
+              to="/streams/new"
+              style={{ textDecoration: 'none', color: '#fff' }}
+            >
+              Create Stream
+            </Link>
+          </Fab>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -29,13 +72,18 @@ class StreamList extends Component {
       <div>
         <h2>Streams</h2>
         <List>{this.renderList()}</List>
+        {this.renderCreate()}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
 export default connect(
