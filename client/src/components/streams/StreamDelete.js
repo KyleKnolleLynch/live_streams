05c0ab8afc1b0b9526ddb481f72548e4a5524f9c
Kyle Lynch <kylelynch104@gmail.com>
2019-06-10 11:03:43 -0800

    version_0.1.0

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import ModalOne from '../ModalOne';
+import Button from '@material-ui/core/Button';
+import history from '../../history';
+
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
-}
+  const actions = (
+    <div>
+      <Button color="secondary">Delete</Button>
+      <Button onClick={() => history.push('/')}>Cancel</Button>
+    </div>
+  )
+
+  const goBack = (
+    () => history.push('/')
+  )
+
+
+
+  return (
+    <div>
+      <ModalOne 
+        title="Delete Stream"
+        modalDesc="Click below to delete this stream"
+        contentDesc="Are you sure you want to delete this stream?"
+        actions={actions}
+        goBack={goBack}
+      />
+    </div>
+  );
+};
 
 export default StreamDelete;
